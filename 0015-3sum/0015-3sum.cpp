e537f3a55bbf715e9756c73be499b7e4fb2e2dfc
SK Irfan <irfan8509111@gmail.com>
2026-09-13 15:20:11 +0530

    Time: 60 ms (17.48%), Space: 28.9 MB (99.54%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,46 +1,60 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
+        
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
 
-        vector<vector<int>> result;
         int n = nums.size();
-        if (n < 3) return result;
+        if (n < 3) return {};
 
+            
         sort(nums.begin(), nums.end());
 
-        for (int i = 0; i < n - 2; i++) {
-            int target = -nums[i];
-            if (target < 0) break;
+        vector<vector<int>> result;
 
-            if (nums[i] + nums[n - 1] + nums[n - 2] < 0) continue;
+        result.reserve(n);
 
-            if (nums[i] + nums[i + 1] + nums[i + 2] > 0) break;
+        const int* base = nums.data();
 
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
+        for (int i = 0; i < n - 2; ++i) {
+            int valI = base[i];
+
+
+            if (valI > 0) break;
+
+            
+            if (i > 0 && valI == base[i - 1]) continue;
+
+            
+            if (valI + base[n - 1] + base[n - 2] < 0) continue;
+
+            
+            if (valI + base[i + 1] + base[i + 2] > 0) break;
 
             int left = i + 1;
             int right = n - 1;
 
             while (left < right) {
-                int sum = nums[left] + nums[right];
+                int sum = valI + base[left] + base[right];
 
-                if (sum == target) {
-                    result.push_back({nums[i], nums[left], nums[right]});
+                if (sum == 0) {
+                    result.push_back({valI, base[left], base[right]});
 
-                    int leftVal = nums[left];
-                    int rightVal = nums[right];
+                    int leftVal = base[left];
+                    int rightVal = base[right];
 
 
-                    while (left < right && nums[left] == leftVal) left++;
-                    while (left < right && nums[right] == rightVal) right--;
-                } else if (sum < target) {
-                    left++;
-                    while (left < right && nums[left] == nums[left - 1]) left++;
-                } else {
-                    right--;
-                    while (left < right && nums[right] == nums[right + 1]) right--;
+                    while (left < right && base[left] == leftVal) ++left;
+                    while (left < right && base[right] == rightVal) --right;
+                } 
+                else if (sum < 0) {
+                    ++left;
+                    while (left < right && base[left] == base[left - 1]) ++left;
+                } 
+                else {
+                    --right;
+                    while (left < right && base[right] == base[right + 1]) --right;
                 }
             }
         }
