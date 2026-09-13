6fe7cf4a6054792757732bc086db637623f0bcd0
SK Irfan <irfan8509111@gmail.com>
2026-09-13 15:16:53 +0530

    Time: 35 ms (98.9%), Space: 28.6 MB (99.91%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -7,13 +7,13 @@ public:
         vector<vector<int>> result;
         int n = nums.size();
         if (n < 3) return result;
-
+        result.reserve(n);
 
         sort(nums.begin(), nums.end());
 
         for (int i = 0; i < n - 2; i++) {
             if (nums[i] > 0) break;
-
+            if (nums[i] + nums[n - 1] + nums[n - 2] < 0) continue;
             if (i > 0 && nums[i] == nums[i - 1]) continue;
 
             int left = i + 1;
@@ -24,11 +24,12 @@ public:
 
                 if (sum == 0) {
                     result.push_back({nums[i], nums[left], nums[right]});
-                    while (left < right && nums[left] == nums[left + 1]) left++;
-                    while (left < right && nums[right] == nums[right - 1]) right--;
 
-                    left++;
-                    right--;
+                    int leftVal = nums[left];
+                    int rightVal = nums[right];
+
+                  while (left < right && nums[left] == leftVal) left++;
+                    while (left < right && nums[right] == rightVal) right--;
                 } else if (sum < 0) {
                     left++;
                 } else {
