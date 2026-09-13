70093e9a929d572acc9588da83885af900f7ac32
SK Irfan <irfan8509111@gmail.com>
2026-09-13 15:11:57 +0530

    Time: 0 ms (100%), Space: 10.4 MB (74.43%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,27 +1,36 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        unordered_map<char, int> roman = {
-            {'I', 1},
-            {'V', 5},
-            {'X', 10},
-            {'L', 50},
-            {'C', 100},
-            {'D', 500},
-            {'M', 1000}
-        };
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
 
         int total = 0;
         int n = s.length();
 
         for (int i = 0; i < n; i++) {
-            if (i < n - 1 && roman[s[i]] < roman[s[i + 1]]) {
-                total -= roman[s[i]];
+            int current = getValue(s[i]);
+            
+            if (i < n - 1 && current < getValue(s[i + 1])) {
+                total -= current;
             } else {
-                total += roman[s[i]];
+                total += current;
             }
         }
 
         return total;
+    }
+
+private:
+    inline int getValue(char c) {
+        switch (c) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
+        }
     }
 };
