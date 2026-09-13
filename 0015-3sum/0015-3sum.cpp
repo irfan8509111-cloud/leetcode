7fec41561ebb2658586627b126369e18845fa427
SK Irfan <irfan8509111@gmail.com>
2026-09-13 15:18:59 +0530

    Time: 43 ms (82.88%), Space: 28.8 MB (99.54%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -7,33 +7,40 @@ public:
         vector<vector<int>> result;
         int n = nums.size();
         if (n < 3) return result;
-        result.reserve(n);
 
         sort(nums.begin(), nums.end());
 
         for (int i = 0; i < n - 2; i++) {
-            if (nums[i] > 0) break;
+            int target = -nums[i];
+            if (target < 0) break;
+
             if (nums[i] + nums[n - 1] + nums[n - 2] < 0) continue;
+
+            if (nums[i] + nums[i + 1] + nums[i + 2] > 0) break;
+
             if (i > 0 && nums[i] == nums[i - 1]) continue;
 
             int left = i + 1;
             int right = n - 1;
 
             while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
+                int sum = nums[left] + nums[right];
 
-                if (sum == 0) {
+                if (sum == target) {
                     result.push_back({nums[i], nums[left], nums[right]});
 
                     int leftVal = nums[left];
                     int rightVal = nums[right];
 
-                  while (left < right && nums[left] == leftVal) left++;
+
+                    while (left < right && nums[left] == leftVal) left++;
                     while (left < right && nums[right] == rightVal) right--;
-                } else if (sum < 0) {
+                } else if (sum < target) {
                     left++;
+                    while (left < right && nums[left] == nums[left - 1]) left++;
                 } else {
                     right--;
+                    while (left < right && nums[right] == nums[right + 1]) right--;
                 }
             }
         }
